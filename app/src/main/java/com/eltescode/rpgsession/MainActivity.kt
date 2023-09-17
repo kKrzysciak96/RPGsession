e36aa1750d4fe311d67fe4890a893c154abf1fe6
krzys <krzysztof.krzysciak@adres.pl>
2023-09-17 11:46:50 +0200

    Init commit.

---
+++ app/src/main/java/com/eltescode/rpgsession/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.eltescode.rpgsession
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.eltescode.rpgsession.ui.theme.RPGSessionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RPGSessionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RPGSessionTheme {
+        Greeting("Android")
+    }
+}
